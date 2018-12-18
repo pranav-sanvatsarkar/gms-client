a634e9b17a92047ce237d3e4873b28d50108aae8
Pranav Sanvatsarkar <pranav.sanvatsarkar@enzigma.com>
2018-12-19 03:19:53 +0530

    Implemented time-zone mapping structure

--- GMS_View/application_js/application.js
+++ GMS_View/application_js/application.js
@@ -1,35 +1,60 @@
-var gmsApp = angular.module('gmsApp',['contactForm']);
-gmsApp.controller('gmsAppController', function($scope,$compile){
-    $scope.value = 'Pronow';
+var gmsApp = angular.module('gmsApp', ['contactForm']);
+gmsApp.controller('gmsAppController', function ($rootScope, $scope, $compile) {
+
+    $rootScope.contacts = [];
+    $rootScope.coordinatedTime = {};
+    $rootScope.coordinatedTime.timezone = {};
     
-    $scope.contacts = [];
-    
+    $rootScope.defaulTimezone = {};
+    $rootScope.defaulTimezone.Name = 'Greenwich Mean Time / Coordinated Universal Time';
+    $rootScope.defaulTimezone.Short_Name__c = 'GMT';
+    $rootScope.defaulTimezone.Region__c = 'Europe';
+    $rootScope.defaulTimezone.City_or_State__c = 'Dublin';
+    $rootScope.defaulTimezone.GMT_Offset_in_Minutes__c = 0;
+    $rootScope.defaulTimezone.Type__c = 'Standard Time';
+    $rootScope.defaulTimezone.Id = 'a001000001dTgV5';
+
+    $rootScope.coordinatedTime.timezone.Name = 'India Standard Time / India Time';
+    $rootScope.coordinatedTime.timezone.Short_Name__c = 'IST';
+    $rootScope.coordinatedTime.timezone.Region__c = 'India';
+    $rootScope.coordinatedTime.timezone.City_or_State__c = '';
+    $rootScope.coordinatedTime.timezone.Country__c = '';
+    $rootScope.coordinatedTime.timezone.GMT_Offset_in_Minutes__c = -330;
+    $rootScope.coordinatedTime.timezone.Type__c = 'Standard Time';
+    $rootScope.coordinatedTime.timezone.Id = 'a001000001dTgUv';
+
+    $rootScope.coordinatedTime.dateTime = new Date();
+    // $rootScope.coordinatedTime.date = new Date();
+
+    $rootScope.$watch('coordinatedTime', function (newValue) {
+        var gmtDateTime = new Date( newValue.dateTime.valueOf() + ( 60000 * newValue.timezone.GMT_Offset_in_Minutes__c ) );
+        for( var index = 0; index < $rootScope.contacts.length; index ++ )
+        {
+            
+        }
+    },true);
+
     $scope.divCounter = 0;
 
-    $scope.addContact = function(){
-        if( $scope.divCounter < 5 )
-        {
+    $scope.addContact = function () {
+        if ($scope.divCounter < 5) {
             var newContactWrapper = $scope.contact();
-            $scope.contacts.push( newContactWrapper );
-            $scope.createContact('div-' + $scope.divCounter);
+            $rootScope.contacts.push(newContactWrapper);
+            $scope.createContact($scope.divCounter);
             $scope.divCounter++;
         }
     }
-    
-    $scope.createContact = function(divId){
+
+    $scope.createContact = function (divId) {
         var div = document.createElement('div');
         div.className = 'schedule-contacts-element';
-        div.id = divId;
+        div.id = 'div-' + divId;
         var innerDiv = document.createElement('div');
-        innerDiv.id = "inner-" + divId;
+        innerDiv.id = "inner-div-" + divId;
         innerDiv.className = 'schedule-contacts-element-content';
         innerDiv.draggable = true;
         innerDiv.setAttribute('ondragstart', 'drag(event)');
-        $scope.contactWrapper = JSON.stringify($scope.contacts[$scope.divCounter].contact);
-
-        var newElement = $compile('<contact-form contact="contactWrapper"/>')($scope);
-        //$scope.$apply();
-        //innerDiv.appendChild();
+        var newElement = $compile('<contact-form contact-id="' + divId + '"/>')($scope);
         div.appendChild(innerDiv);
         angular.element(innerDiv).append(newElement);
         div.setAttribute('ondragover', "allowDrop(event)");
@@ -37,19 +62,94 @@ gmsApp.controller('gmsAppController', function($scope,$compile){
         document.getElementById('schedule-contacts').appendChild(div);
     }
 
-    $scope.contact = function(){
+    $scope.contact = function () {
         var contact = {};
         contact.Id = '123';
-        contact.FirstName = '';
-        contact.Lastname = '';
-        var timeZone = {};
-        timeZone.Id = '';
-        timeZone.Name = '';
+        contact.Name = '';
+
+        // var timezone = {};
+        // timezone.Name = 'India Standard Time / India Time';
+        // timezone.Short_Name__c = 'IST';
+        // timezone.Region__c = 'India';
+        // timezone.City_or_State__c = '';
+        // timezone.Country__c = '';
+        // timezone.GMT_Offset_in_Minutes__c = -330;
+        // timezone.Type__c = 'Standard Time';
+        // timezone.Id = 'a001000001dTgUv';
+
+        // var time = {};
+        // var today = new Date();
+        // time.hours = ( today.getHours() > 12 ? today.getHours() - 12 : today.getHours());
+        // if( today.getHours() > 12 )
+        //     time.meridiem = 'PM';
+        // else
+        //     time.meridiem = 'AM';
+        // time.minutes = today.getMinutes();
+        // time.date = today.getFullYear() + '-' +  ( today.getMonth() + 1 ) + '-' + today.getDate();
+        // time.fullDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+
         var contactWrapper = {};
         contactWrapper.contact = contact;
-        contactWrapper.timeZone = timeZone;
+        contactWrapper.timezone = $rootScope.coordinatedTime.timezone;
+        contactWrapper.dateTime = $rootScope.coordinatedTime.dateTime;
         return contactWrapper;
     }
+
+    $scope.getTimeZones = function () {
+        var timezones = [];
+        var timezone = {};
+        timezone.Name = 'Eastern Standard Time / Eastern Time';
+        timezone.Short_Name__c = 'EST';
+        timezone.Region__c = 'New_York';
+        timezone.City_or_State__c = '';
+        timezone.Country__c = '';
+        timezone.GMT_Offset_in_Minutes__c = -300;
+        timezone.Type__c = 'Standard Time';
+        timezone.Id = 'a001000001dTgUl';
+        timezones.push(timezone);
+
+        timezone = {};
+        timezone.Name = 'Gulf Standard Time';
+        timezone.Short_Name__c = 'GST';
+        timezone.Region__c = 'Asia';
+        timezone.City_or_State__c = '';
+        timezone.Country__c = '';
+        timezone.GMT_Offset_in_Minutes__c = 240;
+        timezone.Type__c = 'Standard Time';
+        timezone.Id = 'a001000001dTgUq';
+        timezones.push(timezone);
+
+        timezone = {};
+        timezone.Name = 'India Standard Time / India Time';
+        timezone.Short_Name__c = 'IST';
+        timezone.Region__c = 'India';
+        timezone.City_or_State__c = '';
+        timezone.Country__c = '';
+        timezone.GMT_Offset_in_Minutes__c = 330;
+        timezone.Type__c = 'Standard Time';
+        timezone.Id = 'a001000001dTgUv';
+        timezones.push(timezone);
+
+        timezone = {};
+        timezone.Name = 'Pacific Standard Time / Pacific Time';
+        timezone.Short_Name__c = 'PST';
+        timezone.Region__c = 'America';
+        timezone.City_or_State__c = 'Tijuana';
+        timezone.Country__c = 'Mexico';
+        timezone.GMT_Offset_in_Minutes__c = -600;
+        timezone.Type__c = 'Daylight Saving Time';
+        timezone.Id = 'a001000001dTFrQ';
+        timezones.push(timezone);
+        return timezones;
+
+
+        // timezone.DST_End_Date__c = '';
+        // timezone.DST_State_Date__c = '';
+        // timezone.DST_Timezone_Offset_in_Minutes__c = '';
+        // timezone.Is_DST_Active__c = '';
+        // timezone.Id = '';
+    }
+    $rootScope.timezones = $scope.getTimeZones();
 });
 
 
@@ -61,8 +161,7 @@ var closeDiv = function (divId) {
 }
 var divCount = 0;
 var addContact = function () {
-    if( divCount < 5 )
-    {
+    if (divCount < 5) {
         createContact('div-' + divCount);
         divCount++;
     }
@@ -98,33 +197,31 @@ var drag = function (event) {
 var drop = function (event) {
     event.preventDefault();
     var elementToMove;
-    
+
     var sourceId = event.dataTransfer.getData('sourceId');
     var sourceElement = document.getElementById(sourceId);
     var sourceElementParent = sourceElement.parentElement;
-    
+
     var destinationId = event.target.id;
     var destinationElement = event.target;
     var destinationElementParent = destinationElement.parentElement;
 
-    if( destinationElement.draggable == false )
-    {
+    if (destinationElement.draggable == false) {
         destinationElement.appendChild(sourceElement);
         sourceElementParent.appendChild(destinationElement.children[0]);
     }
-    else
-    {
+    else {
         destinationElementParent.appendChild(sourceElement);
         sourceElementParent.appendChild(destinationElement);
     }
 }
 
-var readContactHTML = function(){
+var readContactHTML = function () {
     var htmlFile = new XMLHttpRequest();
-    htmlFile.open('GET', 'contact.html',true);
-    htmlFile.onreadystatechange = function(){
-        if( htmlFile.readyState === 4 ){
-            if( htmlFile.status === 200 || htmlFile.status == 0 ){
+    htmlFile.open('GET', 'contact.html', true);
+    htmlFile.onreadystatechange = function () {
+        if (htmlFile.readyState === 4) {
+            if (htmlFile.status === 200 || htmlFile.status == 0) {
                 htmlFile.send(htmlFile.responseText);
             }
         }
--- GMS_View/application_js/contact-form.js
+++ GMS_View/application_js/contact-form.js
@@ -3,16 +3,69 @@ contactForm.directive('contactForm',function(){
     var directive = {};
     directive.restrict = 'E';
     directive.scope = {
-        contact : '=',
-        timezone : '='
+        contactId : '@'
     };
-    //directive.scope.contactWrapper = '=';
     directive.templateUrl = 'application_js/contact-form.html';
-    directive.controller = function($scope){
-        $scope.contact = JSON.parse($scope.contact);
-        console.log($scope.contact);
+    directive.controller = function($scope,$rootScope){
+        $scope.contact = {};
+        $scope.timezones = $rootScope.timezones;
+        $scope.contact = angular.copy($rootScope.contacts[$scope.contactId].contact);
+        //$scope.timezone = angular.copy($rootScope.contacts[$scope.contactId].contact);
+        $scope.selectedTimeZoneId = angular.copy($rootScope.coordinatedTime.timezone.Id);
+        $scope.localDateTime = {};
+        $scope.localDateTime.date = $rootScope.coordinatedTime.dateTime.getFullYear() + '-' +  ( $rootScope.coordinatedTime.dateTime.getMonth() + 1 ) + '-' + $rootScope.coordinatedTime.dateTime.getDate();
+        $scope.localDateTime.hours = ( $rootScope.coordinatedTime.dateTime.getHours() > 12 ? $rootScope.coordinatedTime.dateTime.getHours() - 12 : $rootScope.coordinatedTime.dateTime.getHours()) ;
+        $scope.localDateTime.minutes = $rootScope.coordinatedTime.dateTime.getMinutes();
+        $scope.localDateTime.meridiem = ( $rootScope.coordinatedTime.dateTime.getHours() > 12 ? 'PM' : 'AM');
+        //$scope.localCoordinatedTime = $rootScope.coordinatedTime;
+
+        $scope.$watch('selectedTimeZoneId',function(newValue, oldValue){
+            for(var index = 0; index < $rootScope.timezones.length; index ++)
+            {
+                if( $rootScope.timezones[index].Id === newValue )
+                {
+                    $scope.timezone = $rootScope.timezones[index];
+                    $rootScope.coordinatedTime.timezone = $scope.timezone;
+                    break;
+                }
+            }
+        });
+        $scope.$watch('localDateTime',function(newValue){
+            var dateElements = newValue.date.split('-');
+            var hours = newValue.meridiem == 'PM' ? newValue.hours+12 : newValue.hours;
+            var updatedDateTime = new Date(dateElements[0],dateElements[1],dateElements[2],hours, newValue.minutes);
+
+            var gmtDateTime = new Date( newValue.dateTime.valueOf() + ( 60000 * newValue.timezone.GMT_Offset_in_Minutes__c ) );
+            for( var index = 0; index < $rootScope.contacts.length; index ++ )
+            {
+                
+            }
+
+            //$rootScope.coordinatedTime.dateTime = updatedDateTime;
+            //$rootScope.contacts[$scope.contactId].dateTime = updatedDateTime;
+        },true);
+
+
+        // $scope.$watch('wrapper',function(newValue){
+        //     if( newValue.time )
+        //     {
+        //         $rootScope.coordinatedTime = new Date(newValue.time.fullDate.getFullYear(),newValue.time.fullDate.getMonth(),newValue.time.fullDate.getDate(),(newValue.time.meridiem == 'PM' ? newValue.time.hours + 12 : newValue.time.hours ),newValue.time.minutes );
+        //         var offSetMinutes;
+        //         for( var index = 0; index < $rootScope.timezones.length; index ++ )
+        //         {
+        //             if( newValue.timezone.Id === $rootScope.timezones[index] )
+        //             {
+        //                 offSetMinutes = $rootScope.timezones[index].GMT_Offset_in_Minutes__c;
+        //                 break;
+        //             }
+        //         }
+        //         $rootScope.coordinatedTime = new Date($rootScope.coordinatedTime.valueOf() + ( 60000 * offSetMinutes));
+        //     }
+        // },true);
+        // $scope.$watch('wrapper.timezone',function(newValue){
+        //     //$rootScope.coordinatedTime = new Date(newValue.fullDate.getFullYear(),newValue.fullDate.getMonth(),newValue.fullDate.getDate(),(newValue.meridiem == 'PM' ? newValue.hours + 12 : newValue.hours ),newValue.minutes );
+        //     //console.log($rootScope.coordinatedTime);
+        // });
     }
     return directive;
-});
-contactForm.controller('contactFormController',function($scope){
 });
